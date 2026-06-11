6c3f3281bfab4ed2ceeea2e046aa71fe7a55875f
cnnamani <nnamanichisomnc@gmail.com>
2026-06-11 12:47:39 +0100

    feat: added file server

--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 )
 
 func homePageHandler(writer http.ResponseWriter, request *http.Request) {
+	if request.URL.Path != "/" {
+		// http.NotFound(writer, request)
+		http.Error(writer, "[HP] 404: File Not Found", http.StatusNotFound)
+		return
+	}
 	http.ServeFile(writer, request, "templates/index.html")
 }
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", homePageHandler)
+	fileserver := http.FileServer(http.Dir("./static"))
 
-	http.ListenAndServe("5000", mux)
+	mux.HandleFunc("/", homePageHandler)
+	mux.Handle("/static/", http.StripPrefix("/static/", fileserver))
+
+	fmt.Println("Server is running on port: http://localhost:5500")
+	err := http.ListenAndServe(":5500", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
